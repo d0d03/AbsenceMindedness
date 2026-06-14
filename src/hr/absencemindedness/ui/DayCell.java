962767633aac2ba5b5b9d9079e98a29ad6d61570
d0d03 <dominik.bosnjak33@gmail.com>
2026-06-14 21:53:52 +0200

    prepainting weekends as non working days

--- src/hr/absencemindedness/ui/DayCell.java
+++ src/hr/absencemindedness/ui/DayCell.java
@@ -8,6 +8,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.MonthDay;
 import java.time.format.DateTimeFormatter;
@@ -18,8 +19,9 @@ public class DayCell  extends JPanel {
     private final int year;
     private final int month;
     private final int day;
-    private boolean hovered = false;
     private final CalendarFrame frame;
+    private final boolean isNonWorking;
+    private boolean hovered = false;
 
     public DayCell(CalendarFrame frame, LocalDate selectedDate, String lang){
         Locale locale = lang == null || lang.isBlank() ? Locale.getDefault() : Locale.of(lang);
@@ -27,29 +29,32 @@ public class DayCell  extends JPanel {
         this.day = selectedDate.getDayOfMonth();
         this.year = selectedDate.getYear();
         this.frame = frame;
+        this.isNonWorking = isWeekend(selectedDate);
         setPreferredSize(new Dimension(22,26));
         setOpaque(false);
         setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        setToolTipText(MonthDay.of(month, day).format(DateTimeFormatter.ofPattern("d. MMMM",locale)));
-
-        addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e){
-                if(frame.getSelectedStatus() == DayStatus.NONE){
-                    frame.getCalendarData().remove(new CalendarKey(year, month, day));
-                } else {
-                    frame.getCalendarData().put(new CalendarKey(year, month, day), frame.getSelectedStatus());
+        setToolTipText(selectedDate.format(DateTimeFormatter.ofPattern("d. MMMM",locale)));
+        
+        if(!isNonWorking){
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e){
+                    if(frame.getSelectedStatus() == DayStatus.NONE){
+                        frame.getCalendarData().remove(new CalendarKey(year, month, day));
+                    } else {
+                        frame.getCalendarData().put(new CalendarKey(year, month, day), frame.getSelectedStatus());
+                    }
+                    repaint();
                 }
-                repaint();
-            }
 
-            @Override
-            public void mouseEntered(MouseEvent e) { hovered = true; repaint(); }
+                @Override
+                public void mouseEntered(MouseEvent e) { hovered = true; repaint(); }
 
-            @Override
-            public void mouseExited(MouseEvent e) { hovered = false; repaint(); }
+                @Override
+                public void mouseExited(MouseEvent e) { hovered = false; repaint(); }
 
-        });
+            });
+        }
     }
 
     @Override
@@ -59,6 +64,10 @@ public class DayCell  extends JPanel {
 
         DayStatus status  = getStatus();
         Color base = status == DayStatus.NONE ? AppColors.WHITE : status.getColor();
+
+        if(isNonWorking){
+            base = DayStatus.NON_WORKING.getColor();
+        }
 
         //Fill
         g2.setColor(hovered ? base.darker() : base);
@@ -79,6 +88,10 @@ public class DayCell  extends JPanel {
 
     private DayStatus getStatus() {
        return frame.getCalendarData().getOrDefault(new CalendarKey(year, month, day), DayStatus.NONE);
+    }
+
+    private boolean isWeekend(LocalDate selectedDate){
+        return selectedDate.getDayOfWeek().equals(DayOfWeek.SATURDAY) || selectedDate.getDayOfWeek().equals(DayOfWeek.SUNDAY);
     }
 
 }
