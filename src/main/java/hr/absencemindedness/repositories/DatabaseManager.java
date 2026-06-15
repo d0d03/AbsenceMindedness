eaf4d4df59d8e6dfb823ef8e5ad4aed4a092350d
d0d03 <dominik.bosnjak33@gmail.com>
2026-06-15 15:25:04 +0200

    feat: add AES database encryption and externalize config to application.properties

--- src/main/java/hr/absencemindedness/repositories/DatabaseManager.java
+++ src/main/java/hr/absencemindedness/repositories/DatabaseManager.java
@@ -1,5 +1,7 @@
 package hr.absencemindedness.repositories;
 
+import hr.absencemindedness.config.AppConfig;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -7,7 +9,10 @@ import java.sql.Statement;
 
 public final class DatabaseManager {
 
-    private static final String URL = "jdbc:h2:./absencemindedness;AUTO_SERVER=FALSE";
+    private static final String URL = AppConfig.get("db.url");
+    private static final String USER = AppConfig.get("db.user");
+    private static final String FILE_PASSWORD = AppConfig.get("db.file.password");
+    private static final String USER_PASSWORD = AppConfig.get("db.user.password");
     private static Connection connection;
 
     private DatabaseManager(){
@@ -16,7 +21,7 @@ public final class DatabaseManager {
 
     public static Connection getConnection() throws SQLException{
         if(connection == null || connection.isClosed()){
-            connection = DriverManager.getConnection(URL, "sa", "");
+            connection = DriverManager.getConnection(URL, USER,FILE_PASSWORD + " " + USER_PASSWORD );
         }
         return connection;
     }
@@ -24,14 +29,14 @@ public final class DatabaseManager {
     public static void initialize() throws SQLException {
         try (Statement stmt = getConnection().createStatement()) {
             stmt.execute("""
-                    CREATE TABLE IF NOT EXISTS calendar_entries(
-                        "year" INT,
-                        "month" INT,
-                        "day" INT,
-                        "status" VARCHAR(50),
-                        PRIMARY KEY ("year", "month","day")
-                    )
-                    """);
+                CREATE TABLE IF NOT EXISTS calendar_entries(
+                    "year" INT,
+                    "month" INT,
+                    "day" INT,
+                    "status" VARCHAR(50),
+                    PRIMARY KEY ("year", "month","day")
+                )
+                """);
         }
     }
 
