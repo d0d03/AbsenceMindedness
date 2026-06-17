265f9dcbb826643e4133df77d64cbedd76b3ad5c
d0d03 <dominik.bosnjak33@gmail.com>
2026-06-17 12:52:16 +0200

    fix: package h2 dependency to build use external app.props if present othervise fallback to app.props on classpath

--- src/main/java/hr/absencemindedness/config/AppConfig.java
+++ src/main/java/hr/absencemindedness/config/AppConfig.java
@@ -1,5 +1,7 @@
 package hr.absencemindedness.config;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -9,13 +11,20 @@ public final class AppConfig {
     private static final Properties props = new Properties();
 
     static{
-        try(InputStream in = AppConfig.class.getClassLoader().getResourceAsStream("application.properties")){
-            if (in == null){
-                throw new IllegalStateException("application.properties not found");
+        try{
+            File externalProps = new File(System.getProperty("user.dir") + "/app/classes/application.properties");
+            InputStream in;
+            if (externalProps.exists()){
+                in = new FileInputStream(externalProps);
+            } else {
+                in = AppConfig.class.getClassLoader().getResourceAsStream("application.properties");
             }
+
+            if( in == null ) throw new IllegalStateException("application.properties not found");
             props.load(in);
-        } catch(IOException e){
-            throw new IllegalStateException("Failed to load application.properties", e);
+
+        }catch (IOException e){
+            throw new IllegalStateException("Failed to load application.properties",e);
         }
     }
 
