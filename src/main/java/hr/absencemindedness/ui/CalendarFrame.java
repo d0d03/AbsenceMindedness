2bd9f3ce4e252e09560731bf821290bfc22b15a6
d0d03 <dominik.bosnjak33@gmail.com>
2026-06-29 08:35:11 +0200

    feat: wire holiday loading and display into ClaendarFrame, CalendarGridPanel and DayCell

--- src/main/java/hr/absencemindedness/ui/CalendarFrame.java
+++ src/main/java/hr/absencemindedness/ui/CalendarFrame.java
@@ -1,19 +1,25 @@
 package hr.absencemindedness.ui;
 
+import hr.absencemindedness.config.AppConfig;
 import hr.absencemindedness.constants.AppColors;
 import hr.absencemindedness.enums.DayStatus;
 import hr.absencemindedness.models.CalendarKey;
+import hr.absencemindedness.models.Holiday;
 import hr.absencemindedness.repositories.CalendarRepository;
+import hr.absencemindedness.services.HolidayService;
 
 import javax.swing.*;
 import java.awt.*;
 import java.time.LocalDate;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class CalendarFrame extends JFrame {
 
     private static final String MAIN_TITLE = "AbsenceMindedness";
     private final Map<CalendarKey, DayStatus> calendarData = CalendarRepository.loadAll();
+    private final CalendarGridPanel grid;
+    private Map<LocalDate, Holiday> holidays = Map.of();
 
     private DayStatus selectedStatus = DayStatus.PLANED;
     private int currentYear = LocalDate.now().getYear();
@@ -24,11 +30,12 @@ public class CalendarFrame extends JFrame {
         setLayout(new BorderLayout(0,0));
         getContentPane().setBackground(AppColors.GRID_BG);
 
-        CalendarGridPanel grid = new CalendarGridPanel(() -> currentYear, () -> selectedStatus, calendarData);
+        grid = new CalendarGridPanel(() -> currentYear, () -> selectedStatus, () -> holidays, calendarData);
+        loadHolidaysForYear(currentYear);
 
         HeaderPanel header =  new HeaderPanel(MAIN_TITLE, selectedStatus, year -> {
             currentYear = year;
-            grid.rebuildGrid(currentYear);
+            loadHolidaysForYear(currentYear);
         });
 
         LegendPanel legend = new LegendPanel(selectedStatus, status -> {
@@ -48,4 +55,11 @@ public class CalendarFrame extends JFrame {
         addWindowListener(new AppWindowListener());
 
     }
+
+    private void loadHolidaysForYear(int year){
+        String countryCode = AppConfig.get("app.locale", "HR");
+        HolidayService.loadHolidays(year, countryCode.toUpperCase(), list -> holidays = list.stream().collect(Collectors.toMap(Holiday::date, h-> h)));
+        grid.rebuildGrid(currentYear);
+    }
+
 }
